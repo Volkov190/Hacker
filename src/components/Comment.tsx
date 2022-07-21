712798b8f8a080a24de0a5e70d5cd385da522943
Fedor Volkov <f.volkov10@gmail.com>
2022-07-21 20:23:51 +0300

    delete unnecessary import

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-// import Comment from '../interfaces/Comment';
 import NewsOrComment from '../interfaces/NewsOrComment';
 import { Frame } from './Frame';
 
